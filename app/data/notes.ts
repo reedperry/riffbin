ee40fa6f5efdb85ad146319fa72b505f91c6d655
Reed Perry <reedperry@users.noreply.github.com>
2024-02-10 10:44:58 -0700

    Fix available notes - add octave 0

--- app/data/notes.ts
+++ app/data/notes.ts
@@ -18,7 +18,7 @@ const octaves = [-2, -1, 0, 1, 2, 3, 4];
 let availableNotes: string[] = [];
 for (const octave of octaves) {
   availableNotes = availableNotes.concat(
-    noteNames.map((note) => `${note}${octave ? octave.toString() : ''}`)
+    noteNames.map((note) => `${note}${octave.toString()}`)
   );
 }
 
