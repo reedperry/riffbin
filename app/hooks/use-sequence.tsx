3f35aee8edf30237b6a1480828ea8e244f2fc7f1
Reed Perry <reedperry@users.noreply.github.com>
2024-03-02 20:18:19 -0700

    useSequence: Fixes for updating notes and initialization logic

--- app/hooks/use-sequence.tsx
+++ app/hooks/use-sequence.tsx
@@ -10,36 +10,46 @@ export const defaultDivision: Tone.Unit.Time = '8n';
 
 export function useSequence(props: SequenceProps): void {
   const division = props.division || defaultDivision;
-  const startTime = '+0.05';
+  const startTime = 0;
   const reverb = useRef<Tone.Reverb | null>(null);
-  if (reverb.current === null) {
-    reverb.current = new Tone.Reverb(0.1).toDestination();
-  }
   const synth = useRef<Tone.Synth | null>(null);
-  if (synth.current === null) {
-    synth.current = new Tone.Synth().connect(reverb.current);
-  }
   const sequence = useRef<Tone.Sequence | null>(null);
-  if (sequence.current === null) {
-    sequence.current = new Tone.Sequence(
-      (time, note) => {
-        if (note) {
-          synth.current.triggerAttackRelease(note, division, time);
-        }
-      },
-      props.notes,
-      division
-    ).start(startTime);
-    sequence.current.debug = true;
-  }
+
+  console.log('useSequence');
 
   useEffect(() => {
-    sequence.current.cancel();
-    sequence.current.clear();
-    sequence.current.events = props.notes;
+    if (reverb.current === null && synth.current === null) {
+      reverb.current = new Tone.Reverb(0.1).toDestination();
+      synth.current = new Tone.Synth().connect(reverb.current);
+    }
+    if (sequence.current === null) {
+      sequence.current = new Tone.Sequence(
+        (time, note) => {
+          if (note && synth.current) {
+            synth.current.triggerAttackRelease(note, division, time);
+          }
+        },
+        props.notes,
+        division
+      ).start(startTime);
+      sequence.current.debug = true;
+    }
 
     return () => {
-      sequence.current.dispose();
-    };
+      sequence.current?.dispose();
+      reverb.current?.dispose();
+      synth.current?.dispose();
+      sequence.current = null;
+      synth.current = null;
+      reverb.current = null;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (sequence.current) {
+      sequence.current.cancel();
+      sequence.current.clear();
+      sequence.current.events = props.notes;
+    }
   }, [props.notes]);
 }
